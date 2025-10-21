25c1e76f3c5fb253893601a7189b6ee38888be6c
niyoniyoKanok <kanokphit.niyo@bumail.net>
2025-10-21 16:00:57 +0700

    Ant_Do_a_flip

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -6,17 +6,44 @@ public class Ant : Enemy
     public Transform[] MovePoint;
     public override void Behavior()
     {
-      
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+
+        if (velocity.x < 0 && rb.position.x <= MovePoint[0].position.x)
+        {
+            Flip();
+        }
+       
+        if (velocity.x > 0 && rb.position.x >= MovePoint[1].position.x)
+        {
+            Flip();
+        }
+    }
+
+    public void Flip()
+    {
+        velocity.x *= -1; //change direction of movement
+                          //Flip the image
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Initialize(20);
+        DamageHit = 15;
+
+        velocity = new Vector2(-1.0f,0.0f);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
+    {
+        Behavior();
+    }
+
+        // Update is called once per frame
+        void Update()
     {
         
     }
