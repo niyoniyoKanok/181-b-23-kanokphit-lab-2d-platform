d667b2d56a0cf37e1f210a19ab59351545bebdd3
niyoniyoKanok <kanokphit.niyo@bumail.net>
2025-10-28 16:27:26 +0700

    2d-Platform-Croc&player-Shootable

--- Assets/Scripts/Crocodile.cs
+++ Assets/Scripts/Crocodile.cs
@@ -1,18 +1,16 @@
 using UnityEngine;
 
-public class Crocodile : Enemy
+public class Crocodile : Enemy, IShootable
 {
+    [field: SerializeField] public GameObject Bullet { get; set; }
+    [field: SerializeField] public Transform ShootPoint { get; set; }
+
+    public float ReloadTime { get; set; }
+    public float WaitTime { get; set; }
+    
     [SerializeField] private float atkRange;
+    
     public Player player;
-    public override void Behavior()
-    {
-        Vector2 distance = transform.position - player.transform.position;
-        if (distance.magnitude <= atkRange)
-        {
-            Debug.Log($"{player.name} is in the {this.name}'s atk range!");
-            Shoot();
-        }
-    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,21 +20,34 @@ public class Crocodile : Enemy
         
         atkRange = 6.0f;
         player = GameObject.FindFirstObjectByType<Player>();
-    }
-        // Update is called once per frame
-        void Update()
-    {
-        
-    }
 
-
+        WaitTime = 0.0f;
+            ReloadTime = 5.0f;
+    }
     private void FixedUpdate()
     {
+        WaitTime += Time.fixedDeltaTime;
         Behavior();
+    }
+    public override void Behavior()
+    {
+        Vector2 distance = transform.position - player.transform.position;
+        if (distance.magnitude <= atkRange)
+        {
+          //  Debug.Log($"{player.name} is in the {this.name}'s atk range!");
+            Shoot();
+        }
     }
 
     public void Shoot()
     {
-        Debug.Log($"{this.name} shoots rock to the {player.name}!");
+        if (WaitTime >= ReloadTime)
+        {
+            anim.SetTrigger("Shoot");
+            var bullet = Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
+            Rock rock = bullet.GetComponent<Rock>();
+            rock.InitWeapon(30, this);
+            WaitTime = 0;
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,30 +1,55 @@
 using UnityEngine;
 
-public class Player : Character
+public class Player : Character, IShootable
 {
+    [field: SerializeField] public GameObject Bullet { get; set; }
+    [field: SerializeField] public Transform ShootPoint { get; set; }
+
+    public float ReloadTime { get; set; }
+    public float WaitTime { get; set; }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         base.Initialize(100);
+        ReloadTime = 1.0f;
+        WaitTime = 0.0f;
     }
 
     public void OnHitWith(Enemy enemy)
     {
         TakeDamage(enemy.DamageHit);
     }
-
     private void OnCollisionEnter2D(Collision2D other)
     {
         Enemy enemy = other.gameObject.GetComponent<Enemy>();
-        if (enemy != null) 
-        { 
-         OnHitWith(enemy);  
+        if (enemy != null)
+        {
+            OnHitWith(enemy);
+            Debug.Log($"{this.name} collides with {enemy.name}!");
         }
     }
-
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        WaitTime += Time.deltaTime;
+    }
+
+    private void Update()
+    {
+        Shoot();
+    }
+
+    public void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && WaitTime >= ReloadTime)
+        {
+            var bullet = Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
+            Banana banana = bullet.GetComponent<Banana>();
+            if (banana != null)
+            {
+                banana.InitWeapon(20, this);
+            }
+                WaitTime = 0.0f;
+        }
+
     }
 }
